1ef08e866e700b088b81bde03b25e09061ea3574
salva09 <salva.hg01@gmail.com>
2020-11-24 21:16:21 -0600

    Change modules for components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes, PreloadAllModules, NoPreloading } from '@angular/router';
+import {DiscoverComponent} from './discover/discover.component';
+import {LikedComponent} from './liked/liked.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: '/discover' },
-  { path: 'discover', loadChildren: () => import('./discover/discover.module').then(m => m.DiscoverModule) },
-  { path: 'liked', loadChildren: () => import('./liked/liked.module').then(m => m.LikedModule) },
+  { path: 'discover', component: DiscoverComponent },
+  { path: 'liked', component: LikedComponent },
 ];
 
 @NgModule({
