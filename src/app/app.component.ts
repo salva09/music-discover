8a57393243e71ad0eb8a40e2d9b1fc4f2213a371
salva09 <salva.hg01@gmail.com>
2020-11-24 18:15:39 -0600

    Add freesound API call

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'music-discover';
+
+  constructor() {
+    const search = 'piano';
+    this.httpGetAsync(
+      `https://freesound.org/apiv2/search/text/?query=${search}&token=${process.env.API_KEY}`,
+      (res) => {
+        console.log(res);
+    });
+  }
+
+  httpGetAsync(theUrl, callback): void {
+    const xmlHttp: XMLHttpRequest = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = () => {
+      if (xmlHttp.readyState === 4 && xmlHttp.status === 200) {
+        callback(xmlHttp.responseText);
+      }
+    };
+    xmlHttp.open('GET', theUrl, true); // true for asynchronous
+    xmlHttp.send(null);
+  }
 }
