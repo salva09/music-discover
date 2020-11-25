4cfc9dc155d52f624527364ea18795d8affcd8d6
salva09 <salva.hg01@gmail.com>
2020-11-24 22:42:57 -0600

    Add toolbar for navigation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,25 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatCardModule } from '@angular/material/card';
 import { AppRoutingModule } from './app-routing.module';
+
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
     imports: [
-        BrowserModule,
-        BrowserAnimationsModule,
-        MatCardModule,
-        AppRoutingModule,
+      BrowserModule,
+      BrowserAnimationsModule,
+      MatButtonModule,
+      MatCardModule,
+      AppRoutingModule,
+      MatToolbarModule,
+      MatIconModule,
     ],
   providers: [],
   bootstrap: [AppComponent]
