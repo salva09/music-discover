00da6014b9b3034a3960064772f2b9869b85998f
salva09 <salva.hg01@gmail.com>
2020-11-24 22:43:26 -0600

    Add informative toolbar

--- src/app/discover/discover.module.ts
+++ src/app/discover/discover.module.ts
@@ -1,13 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {DiscoverComponent} from './discover.component';
-
-
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {DiscoverRoutingModule} from './discover-routing.module';
 
 @NgModule({
-  declarations: [DiscoverComponent],
+  declarations: [
+    DiscoverComponent
+  ],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    DiscoverRoutingModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+  ],
 })
 export class DiscoverModule { }
--- src/app/liked/liked.module.ts
+++ src/app/liked/liked.module.ts
@@ -1,13 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {LikedComponent} from './liked.component';
-
-
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {LikedtRoutingModule} from './liked-routing.module';
 
 @NgModule({
-  declarations: [LikedComponent],
+  declarations: [
+    LikedComponent
+  ],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    LikedtRoutingModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+  ],
 })
 export class LikedModule { }
