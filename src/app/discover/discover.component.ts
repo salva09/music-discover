54de52ba2b61c5b61b8486958b74641436f55544
salva09 <salva.hg01@gmail.com>
2020-11-26 01:13:23 -0600

    Add freesound API and simple request

--- src/app/discover/discover.component.ts
+++ src/app/discover/discover.component.ts
@@ -6,10 +6,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./discover.component.css']
 })
 export class DiscoverComponent implements OnInit {
+  results: any;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
   }
 
+  ngOnInit(): void {
+    let query = 'alarm&fields=name,previews';
+    query = query.concat(`&token=DTkKJVf5GktLFuU8I21pyUzqlEdn2pPaTY1f7kck`);
+
+    this.httpGetAsync(
+      `https://freesound.org/apiv2/search/text/?query=${query}`,
+      (res) => {
+        const json = JSON.parse(res);
+
+        this.results = json.results;
+      });
+  }
+
+  httpGetAsync(theUrl, callback): void {
+    const xmlHttp: XMLHttpRequest = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = () => {
+      if (xmlHttp.readyState === 4 && xmlHttp.status === 200) {
+        callback(xmlHttp.responseText);
+      }
+    };
+    xmlHttp.open('GET', theUrl, true); // true for asynchronous
+    xmlHttp.send(null);
+  }
 }
